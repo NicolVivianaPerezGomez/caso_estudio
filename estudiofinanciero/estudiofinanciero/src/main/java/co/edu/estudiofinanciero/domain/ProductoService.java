e8c2201e488a9ad27956b9d80a0434dcf720bc9d
Nicol Viviana Pérez Gómez <nperez@uniempresarial.edu.co>
2024-11-05 12:43:30 -0500

    Add files via upload

---
+++ estudiofinanciero/estudiofinanciero/src/main/java/co/edu/estudiofinanciero/domain/ProductoService.java
@@ -0,0 +1,16 @@
+package co.edu.estudiofinanciero.domain;
+
+import java.util.List;
+
+public interface ProductoService {
+
+    List<Producto> listar();
+
+    Producto listarId(int id);
+
+    Producto add(Producto p);
+
+    Producto edit(Producto p);
+
+    Producto delete(int id);
+}
